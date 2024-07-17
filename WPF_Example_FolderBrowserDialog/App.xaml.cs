5c0be22fab71e9629229a83f0ab890cd01d6688d
tpaks <tpaks@DESKTOP-1NL0OLQ>
2024-07-17 22:07:56 +0900

    초기 커밋.

---
+++ WPF_Example_FolderBrowserDialog/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Configuration;
+using System.Data;
+using W = System.Windows;
+//using WPF = System.Windows.Application;
+//using Winforms = System.Windows.Forms.Application;
+
+namespace WPF_Example_FolderBrowserDialog
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : W.Application
+    {
+    }
+
+}
---
+++ WPF_Example_FolderBrowserDialog/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WinForms = System.Windows.Forms;
+
+namespace WPF_Example_FolderBrowserDialog
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFrie_Click(object sender, RoutedEventArgs e)
+        {
+            WinForms.FolderBrowserDialog dialog = new WinForms.FolderBrowserDialog();
+            dialog.InitialDirectory = "C:\\Users\\tpaks\\source\\repos";
+            WinForms.DialogResult result = dialog.ShowDialog();
+            
+            if(result == WinForms.DialogResult.OK)
+            {
+                string folder = dialog.SelectedPath;
+            }
+
+        }
+    }
+}
